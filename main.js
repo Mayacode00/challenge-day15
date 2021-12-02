1c8d58850fc3d249512b90cf89f78509dc74eb65
Mariam Ayoade <92252531+Mayacode00@users.noreply.github.com>
2021-12-02 17:22:55 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const loanForm = document.getElementById("loan-form");
+
+loanForm.addEventListener("submit", calculateResults);
+
+const amount = document.getElementById("amount");
+const interest = document.getElementById("interest");
+const years = document.getElementById("years");
+const monthlyPayment = document.getElementById("monthly-payments");
+const totalInterests = document.getElementById("total-interests");
+const totalAmount = document.getElementById("total-amounts");
+
+function calculateResults(e) {
+  e.preventDefault();
+  console.log("calculating");
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayment = parseFloat(years.value) * 12;
+
+  const x = Math.pow(1 + calculatedInterest, calculatedPayment);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (monthly) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalAmount.value = (monthly * calculatedPayment).toFixed(2);
+    totalInterests.value = (monthly * calculatedPayment - principal).toFixed(2);
+  } else {
+    console.log("please check your numbers and try again");
+  }
+
+  function showError(error) {
+    const errorDiv = document.createElement("div");
+
+    const card = document.querySelector(".card");
+    const heading = document.querySelector(".headings");
+
+    errorDiv.className = "alert alert-danger";
+    errorDiv.appendChild(document.createTextNode(error));
+    card.insertBefore(errorDiv, heading);
+  }
+}
